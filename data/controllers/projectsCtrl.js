81d08c5754f7f891019c4fd14cf643908a362903
Juan Rodriguez <juanrodriguez@juanrod.local>
2015-11-23 22:31:18 -0600

    completed website v 1.0

--- data/controllers/projectsCtrl.js
+++ data/controllers/projectsCtrl.js
@@ -5,20 +5,20 @@ app.controller("projectsCtrl",
   	$scope.projectContent = [
   			{ 
   				Name: "uPick",
-  				Skills: "Made the whole damn thing myself!",
-  				Description: "Mumblecore +1 helvetica scenester four dollar toast. Cray blog organic, cold-pressed migas brunch chambray leggings actually microdosing. Locavore raw denim slow-carb, keffiyeh flexitarian banh mi mlkshk ugh gentrify waistcoat cronut small batch helvetica DIY. Tofu kickstarter chillwave, pop-up keytar crucifix tacos iPhone shoreditch everyday carry williamsburg twee. Celiac bitters listicle kitsch, ethical scenester locavore helvetica aesthetic cold-pressed. Try-hard marfa whatever raw denim bespoke, franzen wolf authentic selvage. ",
+  				Skills: "Angular, Bootstrap, Google Maps API, Google Places API",
+  				Description: "uPick, a mobile-friendly web application, is a solution for my personal struggle to pick a restaurant. With the implementation of geolocation, a number of local restaurants populate on the screen. With a click of a button, a restaurant of your choice is randomly choosen.",
   				Img: "data/img/uPickMockup.png"
   			},
   			{
   				Name: "Music History",
-  				Skills: "Used Angular and Bootstrap!",
-  				Description: "Mumblecore +1 helvetica scenester four dollar toast. Cray blog organic, cold-pressed migas brunch chambray leggings actually microdosing. Locavore raw denim slow-carb, keffiyeh flexitarian banh mi mlkshk ugh gentrify waistcoat cronut small batch helvetica DIY. Tofu kickstarter chillwave, pop-up keytar crucifix tacos iPhone shoreditch everyday carry williamsburg twee. Celiac bitters listicle kitsch, ethical scenester locavore helvetica aesthetic cold-pressed. Try-hard marfa whatever raw denim bespoke, franzen wolf authentic selvage.",
+  				Skills: "Angular, Material Design, Firebase",
+  				Description: "An in-class, on-going assignment that put what we learned to practice. The overarching goal was to practice various ways to implement new languages, frameworks, and libraries for adding, editing, deleting, and storing data. From vanilla Javascript, jQuery, Requirejs, loDash, underscore, Handlebars, Firebase and Angularjs.",
   				Img: "data/img/bey2.jpg"
   			},
   			{
   				Name: "Faux Pinterest",
-  				Skills: "Worked on a team. My job was mainly visual.",
-  				Description:"Mumblecore +1 helvetica scenester four dollar toast. Cray blog organic, cold-pressed migas brunch chambray leggings actually microdosing. Locavore raw denim slow-carb, keffiyeh flexitarian banh mi mlkshk ugh gentrify waistcoat cronut small batch helvetica DIY. Tofu kickstarter chillwave, pop-up keytar crucifix tacos iPhone shoreditch everyday carry williamsburg twee. Celiac bitters listicle kitsch, ethical scenester locavore helvetica aesthetic cold-pressed. Try-hard marfa whatever raw denim bespoke, franzen wolf authentic selvage. ",
+  				Skills: "Bootstrap, Angularjs, Firebase",
+  				Description:"A two day group project to make a Pinterest clone with Angularjs. My main focus was user experience and design of the project.",
   				Img: "data/img/bey1.jpg"
   			}
   	];
@@ -29,9 +29,13 @@ app.controller("projectsCtrl",
   $scope.currentProject = $scope.projectContent[projectIndex];
 
   $scope.nextProject = function() {
+
       projectIndex = projectIndex+1;
       this.currentProject = $scope.projectContent[projectIndex];
-        
+      if(projectIndex > 2){
+        projectIndex = -1;
+      }
+    console.log("click", projectIndex);    
    };
   $scope.prevProject = function() {
      projectIndex = projectIndex-1;
