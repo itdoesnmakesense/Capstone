f8c9027ce27c24d13855fab3e8a8a275f0363400
Juan Rodriguez <juanrodriguez@juanrod.local>
2015-12-29 13:24:10 -0600

    added skroll capabilities to the skills partial

--- data/controllers/main.js
+++ data/controllers/main.js
@@ -2,9 +2,9 @@ app.controller("MainCtrl",
   ["$scope",
   function($scope, $rootScope) {
 
-var s = skrollr.init();
+// var s = skrollr.init();
 
-console.log("skrollr exists", s);
+//console.log("skrollr exists", s);
 console.log( "App Loaded!", $scope );
 
 
