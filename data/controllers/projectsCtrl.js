b127c180e2b60d70026b357cec03db97a59913ff
Juan Rodriguez <juanrodriguez@juanrod.local>
2016-01-17 10:43:03 -0600

    new projects template

--- data/controllers/projectsCtrl.js
+++ data/controllers/projectsCtrl.js
@@ -2,26 +2,42 @@ app.controller("projectsCtrl",
   ["$scope", 
   function($scope) {
 
-  	$scope.projectContent = [
-  			{ 
-  				Name: "uPick",
-  				Skills: "AngularJS, Bootstrap, Google Maps API",
-  				Description: "uPick, a mobile-friendly web application, is a solution for my personal struggle to pick a restaurant. With the implementation of geolocation, a number of local restaurants populate on the screen. With a click of a button, a restaurant of your choice is randomly choosen.",
-  				Img: "data/img/uPickMockup.png"
-  			},
-  			{
-  				Name: "Music History",
-  				Skills: "AngularJS, Material Design, Firebase",
-  				Description: "An in-class, on-going assignment that put what we learned to practice. The overarching goal was to practice various ways to implement new languages, frameworks, and libraries for adding, editing, deleting, and storing data. From vanilla Javascript, jQuery, Requirejs, loDash, underscore, Handlebars, Firebase and Angularjs.",
-  				Img: "data/img/bey2.jpg"
-  			},
-  			{
-  				Name: "Faux Pinterest",
-  				Skills: "Bootstrap, AngularJS, Firebase",
-  				Description:"A two day group project to make a Pinterest clone with Angularjs. My main focus was user experience and design of the project.",
-  				Img: "data/img/bey1.jpg"
-  			}
-  	];
+    $scope.projectContent = [
+      {
+        Title: "uPick",
+        Skills: "MEAN Stack, Google Maps & Places API, Bootstrap",
+        Description: "uPick, a mobile-friendly web application, is a solution for my personal struggle to pick a restaurant. With the implementation of geolocation, a number of local restaurants populate on the screen. With a click of a button, a restaurant of your choice is randomly choosen.",
+        Img: "data/img/uPick2.png"
+      },
+       {
+        Title: "Something about why",
+        Skills: "more skills",
+        Description: "explain how",
+        Img: "data/img/uPick1.png"
+      }
+
+    ];
+
+  	// $scope.projectContent = [
+  	// 		{ 
+  	// 			Name: "uPick",
+  	// 			Skills: "AngularJS, Bootstrap, Google Maps API",
+  	// 			Description: "uPick, a mobile-friendly web application, is a solution for my personal struggle to pick a restaurant. With the implementation of geolocation, a number of local restaurants populate on the screen. With a click of a button, a restaurant of your choice is randomly choosen.",
+  	// 			Img: "data/img/uPick_gifs/uPick_homePage.gif"
+  	// 		},
+  	// 		{
+  	// 			Name: "uPick_v2",
+  	// 			Skills: "MongoDB, ExpressJS, AngularJS, NodeJS, Google Maps API",
+  	// 			Description: "uPick version 2 using the MEAN Stack with API implementation.",
+  	// 			Img: "data/img/uPick_gifs/uPick_second_Scroll.gif"
+  	// 		},
+  	// 		{
+  	// 			Name: "uPick_v2: another look",
+  	// 			// Skills: "Bootstrap, AngularJS, Firebase",
+  	// 			// Description:"A two day group project to make a Pinterest clone with Angularjs. My main focus was user experience and design of the project.",
+  	// 			Img: "data/img/uPick_gifs/uPick_move.gif"
+  	// 		}
+  	// ];
 
 
 
--- data/directives/projectFormat.js
+++ data/directives/projectFormat.js
@@ -2,7 +2,8 @@ app.directive("projectFormat", function() {
 	return{
 		restrict: 'E',
 		templateUrl: "data/partials/projectsFormat.html",
-		replace: true,
+		replace: true
+		
 		
 
 };
